4c5a6edfaa03bcd04f3d6444c504a5007f5c9c18
nguyenhao3209 <haontce180451@fpt.edu.vn>
2025-10-12 16:09:27 +0700

    public key

---
+++ src/main/java/com/chatbox/chatbox/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.chatbox.chatbox.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://greenshieldmekongvn.vercel.app/")
+                        .allowedMethods("GET", "POST")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
